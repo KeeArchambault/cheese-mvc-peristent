6b20ac2b099d123fa34759aca655f7c934f435c9
Kee Archambault <keeandraa@gmail.com>
2019-09-09 19:35:13 -0500

    started implementing menus, many-to-many relationship with cheeses

---
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -0,0 +1,31 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.Menu;
+import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping(value="menu")
+public class MenuController {
+
+    @Autowired
+    private MenuDao menuDao;
+
+    @Autowired
+    private CheeseDao cheeseDao;
+
+    @RequestMapping(value="")
+    public String index(Model model){
+
+        model.addAttribute("menus", menuDao.findAll());
+
+        return "menu/index";
+    }
+}
---
+++ src/main/java/org/launchcode/models/Menu.java
@@ -0,0 +1,48 @@
+package org.launchcode.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+public class Menu {
+
+
+    @NotNull
+    @Size(min=3, max=15)
+    private String name;
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @ManyToMany
+    List<Cheese> cheeses;
+
+    public Menu(){
+    }
+
+    public Menu(String name){
+    }
+
+    public void addItem(Cheese item){
+        cheeses.add(item);
+    }
+
+    public List<Cheese> getCheeses() {
+        return cheeses;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
